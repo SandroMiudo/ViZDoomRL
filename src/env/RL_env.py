61dcdb43e41d2c06bd1af0c09355d0e463fe3cf4
SandroMiudo <sandromiudo0@gmail.com>
2024-07-01 15:02:37 +0200

    extended basic concept

---
+++ src/env/RL_env.py
@@ -0,0 +1,53 @@
+import os
+import vizdoom as vzd
+from random import choice
+from time import sleep
+
+class RL_Env:
+   
+   def __init__(self, cfg : str) -> None:
+    self.game = game = vzd.DoomGame()
+    game.load_config(os.path.join(vzd.scenarios_path, "deadly_corridor.cfg")) # or any other scenario file
+    self.buttons_supported = game.get_available_buttons()
+    game.init()
+
+   def get_game_state(self):
+    return self.game.get_state()
+   
+   def get_episode_time(self):
+    return self.game.get_episode_time()
+   
+   def perform_action(self, action):
+    assert len(action) >= 1 and len(action[0]) == len(self.buttons_supported) , f"Actions array have to match Buttons array! expected : \
+        {self.buttons_supported}, but received : {len(action[0])}" 
+    if self.game.is_episode_finished():
+        self.game.new_episode()
+    return self.game.make_action(action) # return reward
+   
+   def get_env_shape(self):
+     return (self.game.get_screen_height(), self.game.get_screen_width(), self.get_screen_channels())
+
+   def close_env(self):
+    self.game.close()
+
+"""actions = [[True, True, True, True, True, True, True], [True, True, True, True, True, True, False], [True, True, True, False, True, True, True],
+           [False, True, False, True, True, False, False], [True, False, True, False, True, True, False], [False, False, True, True, True, True, True], 
+           [True, True, True, True, True, True, False]]
+
+import gymnasium as gym
+from vizdoom import gymnasium_wrapper # This import will register all the environments
+
+env = gym.make("VizdoomCorridor-v0", render_mode="human") # or any other environment id
+observation, infos = env.reset()
+
+print("", env.action_space, env.observation_space)
+
+for __ in range(1000):
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    print("action : ", action)
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close() """
